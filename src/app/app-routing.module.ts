586ad3be35e969439b9d58877860077cfbe66eae
Mohamed IDBRAHIM <idbrahimdev@gmail.com>
2019-06-22 11:37:22 +0200

    generate components and services

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,21 @@
+import { ProductEditComponent } from './components/product-edit/product-edit.component';
+import { PageNotFoundComponent } from './components/page-not-found/page-not-found.component';
+import { LoginComponent } from './components/login/login.component';
+import { RegisterComponent } from './components/register/register.component';
+import { ProductAddComponent } from './components/product-add/product-add.component';
+import { ProductListComponent } from './components/product-list/product-list.component';
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: "", redirectTo: '/products', pathMatch: 'full'},
+  { path: "products", component: ProductListComponent },
+  { path: "products/add", component: ProductAddComponent },
+  { path: "products/edit/:id", component: ProductEditComponent },
+  { path: "register", component: RegisterComponent },
+  { path: "login", component: LoginComponent },
+  { path: "**", component: PageNotFoundComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,25 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
+import { ProductListComponent } from './components/product-list/product-list.component';
+import { ProductAddComponent } from './components/product-add/product-add.component';
+import { ProductEditComponent } from './components/product-edit/product-edit.component';
+import { LoginComponent } from './components/login/login.component';
+import { RegisterComponent } from './components/register/register.component';
+import { PageNotFoundComponent } from './components/page-not-found/page-not-found.component';
 
 @NgModule({
   declarations: [
-    AppComponent
-  ],
+    AppComponent,
+    NavbarComponent,
+    ProductListComponent,
+    ProductAddComponent,
+    ProductEditComponent,
+    LoginComponent,
+    RegisterComponent,
+    PageNotFoundComponent  
+],
   imports: [
     BrowserModule,
     AppRoutingModule
